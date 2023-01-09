4a14beb13a9026a18e5dfb255522b9aadd844a57
Ahmet Denizli <ahmetdenizli@hotmail.com.tr>
2023-01-09 03:26:18 +0300

    Update add_pl_log.php

--- pl_admin/add_pl_log.php
+++ pl_admin/add_pl_log.php
@@ -14,50 +14,74 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $db->where('pl_id', $pl_id);
     $row2 = $db->getOne('parking_lot');
 
+    $current_time = strtotime( date('y-m-d H:i:s', strtotime('+2 hours')));
+
     $data_to_update['enter_date'] = str_replace("T", " ", $data_to_update['enter_date']);
-    $data_to_update['exit_date'] = str_replace("T", " ", $data_to_update['exit_date']);
-
-    $phpdate1 = strtotime( $data_to_update['enter_date'] );
-    $phpdate2 = strtotime( $data_to_update['exit_date'] );
-
-    if (!$row2 || $phpdate1 > $phpdate2) {
-
-        $_SESSION['failure'] = "Giriş tarihi çıkış tarihinden önce olmalı!";
-
+    if (strtotime( $data_to_update['enter_date']) > $current_time ) {
+        $_SESSION['failure'] = "Giriş Tarihi doğru girilmeli";
         header('location: pl_log.php?pl_id='.$pl_id);
         exit;
-    } 
+    }
 
-    //Check whether the user name already exists ;
-    $db = getDbInstance();
-
-    $data = Array (
-        'cpl_pl_id' => $pl_id,
-        'cpl_car_plate' => $data_to_update['car_plate'],
-        'cpl_enter_date' => $data_to_update['enter_date'],
-        'cpl_exit_date' => $data_to_update['exit_date'],
-        'cpl_total_payment' => (($phpdate2-$phpdate1)/3600)*$row2['pl_hourly_rate'],
-    );
-
-    $id = $db->insert ('car_parking_logs', $data);
-    if ($id) {
-        $_SESSION['success'] = "Otopark kaydı başarılı bir şekilde eklendi";
+    if($data_to_update['exit_date']){
+        $data_to_update['exit_date'] = str_replace("T", " ", $data_to_update['exit_date']);
+    
+        $phpdate1 = strtotime( $data_to_update['enter_date'] );
+        $phpdate2 = strtotime( $data_to_update['exit_date'] );
+    
+        if (!$row2 || $phpdate1 > $phpdate2 || $phpdate2 > $current_time) {
+    
+            $_SESSION['failure'] = "Giriş tarihi ve çıkış tarihi doğru girilmeli!";
+    
+            header('location: pl_log.php?pl_id='.$pl_id);
+            exit;
+        } 
+    
+        //Check whether the user name already exists ;
+        $db = getDbInstance();
+    
+        $data = Array (
+            'cpl_pl_id' => $pl_id,
+            'cpl_car_plate' => $data_to_update['car_plate'],
+            'cpl_enter_date' => $data_to_update['enter_date'],
+            'cpl_exit_date' => $data_to_update['exit_date'],
+            'cpl_total_payment' => (($phpdate2-$phpdate1)/3600)*$row2['pl_hourly_rate'],
+        );
+    
+        $id = $db->insert ('car_parking_logs', $data);
+        if ($id) {
+            $_SESSION['success'] = "Otopark kaydı başarılı bir şekilde eklendi";
+        } else {
+            $_SESSION['failure'] = "Ekleme yapılırken hata alındı: " . $db->getLastError();
+        }
     } else {
-        $_SESSION['failure'] = "Ekleme yapılırken hata alındı: " . $db->getLastError();
+        $data = Array (
+            'cpl_pl_id' => $pl_id,
+            'cpl_car_plate' => $data_to_update['car_plate'],
+            'cpl_enter_date' => $data_to_update['enter_date']
+        );
+
+        $db = getDbInstance();
+        $db->startTransaction();
+        $db->where('pl_id', $pl_id);
+        $row3 = $db->update('parking_lot', Array ('pl_size' => $db->inc(1)));
+
+        $id = $db->insert('car_parking_logs', $data);
+
+        if ($row3 && $id) {
+            $db->commit();
+            
+            $_SESSION['success'] = "Otopark kaydı başarılı bir şekilde eklendi";
+        } else {
+            $db->rollback();
+            $_SESSION['failure'] = "Otopark kapasitesi dolu";
+        }
     }
 
     header('location: pl_log.php?pl_id='.$pl_id);
     exit;
 
 }
-
-$totalCapacity = 10000;
-
-//$db = getDbInstance();
-//$cities = $db->get("cities");
-$cities = array( array("city_id" => "34" , "city_name" => "İstanbul"));
-$db = getDbInstance();
-$districts = $db->get("districts");
 
 // import header
 require_once 'includes/header.php';
@@ -86,7 +110,7 @@ require_once 'includes/header.php';
 
         <div class="form-group">
             <label for="exit_date">Otopark Çıkış Tarihi</label>
-            <input type="datetime-local" name="exit_date" class="form-control" required="required" id="exit_date">
+            <input type="datetime-local" name="exit_date" class="form-control" id="exit_date">
         </div>
 
         <div class="form-group text-center">
